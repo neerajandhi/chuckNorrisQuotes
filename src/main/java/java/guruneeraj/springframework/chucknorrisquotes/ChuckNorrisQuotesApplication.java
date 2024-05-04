f88b5d0b69de81ccd774069afbf329cba1221b89
n0k00bn <neerajandhi@gmail.com>
2024-05-04 22:39:07 +0530

    Adding Chuck Norris Application

---
+++ src/main/java/java/guruneeraj/springframework/chucknorrisquotes/ChuckNorrisQuotesApplication.java
@@ -0,0 +1,13 @@
+package java.guruneeraj.springframework.chucknorrisquotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChuckNorrisQuotesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChuckNorrisQuotesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/java/guruneeraj/springframework/chucknorrisquotes/ChuckNorrisQuotesApplicationTests.java
@@ -0,0 +1,13 @@
+package java.guruneeraj.springframework.chucknorrisquotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChuckNorrisQuotesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
